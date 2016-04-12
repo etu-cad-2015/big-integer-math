54e130f0090dac5f5112c4182f301c58cf403380
paulbilkis <jew.bilkis@gmail.com>
2016-04-12 23:48:06 +0300

    Added new module add_zz_z

---
+++ add_zz_z.js
@@ -0,0 +1,27 @@
+function ADD_ZZ_Z(int A, int B){
+  var a=POZ_Z_D(A);
+  var b=POZ_Z_D(B);
+  if ((a!=1)||(b!=1))
+    return(ADD_NN_N(A,B));
+  if ((a!=2)||(b!=2)){
+    MUL_Z-_Z(A);
+    MUL_Z-_Z(B);
+    return(MUL_Z-_Z(ADD_NN_N(A,B)); 
+  }
+  else{
+    if (a==1){
+      if (COM_NN_D(ABS_Z_N(A,B)==2)
+	return(MUL_Z-_Z(SUB_NN_N(ABS_Z_N(A),B);
+      else
+  	return(SUB_NN_N(B,ABS_Z_N(A))
+    }
+    else
+      if (COM_NN_D(ABS_Z_N(B,A)==2)
+	return(MUL_Z-_Z(SUB_NN_N(ABS_Z_N(B[m]),A[n]);
+      else
+  	return(SUB_NN_N(B[m],ABS_Z_N(A[n]))
+}
+  
+        
+
+  	
