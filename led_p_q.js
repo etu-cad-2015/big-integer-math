cfc808a99dbabd4bded86913afd27c8d2995a49c
EgoJonny <kovrigin.v.k@gmail.com>
2016-04-14 22:30:14 +0300

    New modul

---
+++ led_p_q.js
@@ -0,0 +1,10 @@
+/*
+	Ковригин Владимир Константинович
+	Группа № 5302
+	Модуль P-5 Старший коэффицент многочлена
+*/  
+	Mathlib.led_p_q = function  (polynomial) {	
+	  
+	  return polynominal[ Mathlib.deg_p_n(polynominal)];
+	  
+	}	
