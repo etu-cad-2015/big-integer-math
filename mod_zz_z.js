97c29a9a71b15ca3b24d3e03edc5ec07052c6a10
Artyom <shpager@gmail.com>
2016-04-14 23:00:35 +0300

    mod_zz_z.js

--- mod_zz_z.js
+++ mod_zz_z.js
@@ -10,15 +10,11 @@ MathLib.mod_zz_z = function(zA, zB)
 	A = MathLib.cloneNumber(zA);
 	B = MathLib.cloneNumber(zB);
 	
-	if (B.d != 0)     //Если делитель не равен нулю
-	{
-	  if (A.s != B.s)     //Сравниваем их знаки
-	    {
-		    return MathLib.mul_z-_z(MathLib.sub_zz_z(A.d, MathLib.mul_zz_z(B.d, MathLib.div_zz_z(A.d, B.d)))); 	//Если не одинаковые,
-		                                                                            				//то возвращает значение умноженное на (-1)
-		  }
-		else return MathLib.sub_zz_z(A.d, MathLib.mul_zz_z(B.d, MathLib.div_zz_z(A.d, B.d)));   //Иначе, возвращает разность между A.d и B.d,
-		                                                                  			//умноженным на целое частное между A.d и B.d
-	}
-	else return 0; 		//Вернуть 0, если делитель равен 0
+	if (A.s != B.s)     //Сравниваем их знаки
+		{
+		return MathLib.mul_z-_z(MathLib.sub_zz_z(A, MathLib.mul_zz_z(B, MathLib.div_zz_z(A, B)))); 	//Если не одинаковые,
+		                                                                            			//то возвращает значение умноженное на (-1)
+		}
+	else return MathLib.sub_zz_z(A, MathLib.mul_zz_z(B, MathLib.div_zz_z(A, B)));   	//Иначе, возвращает разность между A.d и B.d,
+		                                                                  		//умноженным на целое частное между A.d и B.d
 }
