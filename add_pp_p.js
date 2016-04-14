00ddddaf1fa2669831aa26359f36a286e92698d8
Asset34 <artemiy.kashintsev@mail.ru>
2016-04-14 23:39:34 +0300

    Update add_pp_p.js

--- add_pp_p.js
+++ add_pp_p.js
@@ -6,17 +6,17 @@ MathLib.add_pp_p = function (polinom1, polinom2) {
       //суммарный многочлен
       var polinomsum = [];
 	  
-	  //Нахождение длины наибольшего многочлена (кол-во итераций будущего цикла)
-	  var maxlength = Math.max(polinom1.length, polinom2.length);
+      //Нахождение длины наибольшего многочлена (кол-во итераций будущего цикла)
+      var maxlength = Math.max(polinom1.length, polinom2.length);
 	  
 	  for (var i = 0; i < maxlength; ++i) {
 	        //Добавление нового элемента в массив коэффициентов полинома polinomsum 
 	        polinomsum.push ([ { d: [], s: 1 }, { d: [] } ]); 
-			//Вычисление i-ого коэффициента суммарного многочлена
-			polinomsum[i] = MathLib.add_qq_q (polinom1[i], polinom2[i]);
+		//Вычисление i-ого коэффициента суммарного многочлена
+		polinomsum[i] = MathLib.add_qq_q (polinom1[i], polinom2[i]);
 	        }
 			
-	  return polinomsum;	
+      return polinomsum;	
 }
 
 
