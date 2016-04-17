e99c5d02772483ba3f35db29e69ad2d44d133f92
Pipfstarrd <pipfstarrd@openmailbox.org>
2016-04-17 12:08:10 +0000

    Closed a pull request

--- lcm_nn_n.js
+++ lcm_nn_n.js
@@ -4,6 +4,7 @@
 // -- Раков Константин, 5302.
 MathLib.lcm_nn_n = function(numb1, numb2)
 {
-	// НОК = (numb1 * numb2) / НОД(numb1, numb2)
-	return MathLib.div_nn_n(MathLib.mul_nn_n(numb1, numb2), MathLib.gcf_nn_n(numb1, numb2));
+  // НОК = (numb1 * numb2) / НОД(numb1, numb2)
+  return MathLib.div_nn_n(MathLib.mul_nn_n(numb1, numb2), 
+                          MathLib.gcf_nn_n(numb1, numb2));
 };
