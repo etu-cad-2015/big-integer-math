74f1714957526074e792b46fec21e68a9bb2839f
Pipfstarrd <pipfstarrd@openmailbox.org>
2016-04-19 15:49:46 +0000

    Forgot to git add the files

---
+++ mul_nk_n.js
@@ -0,0 +1,25 @@
+/* 
+ * Автор: Pipfstarrd
+ * Группа: 5302
+ * Назначение: умножает число в p-ичной системе на p^k (сдвигает на k разрядов)
+ * Входные данные: число и степень k
+ * Выходные данные: число со сдвигом на k разрядов
+ */
+
+MathLib.mul_nk_n = function(number, k) {
+  var c = {};
+  c.d = [];
+  for (var i = number.d.lenth; i >= 0; i--) {
+    c.d[i + k] = number.d[i];
+  }
+  
+  for (var i = k - 1; i >= 0; i--) {
+    c.d[i] = 0;
+  }
+  
+  c.s = number.s;
+  
+  // MathLib.normalize is not needed as we are 100% shure that 
+  // the most significant digit != 0
+  return c;
+}
