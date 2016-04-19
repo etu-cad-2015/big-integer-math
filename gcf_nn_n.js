99f9635353693a6ca8892f2779a03bc04611eb37
Pipfstarrd <pipfstarrd@openmailbox.org>
2016-04-19 15:26:58 +0000

    Implemented mul_nk_n.js

--- gcf_nn_n.js
+++ gcf_nn_n.js
@@ -1,7 +1,7 @@
 // Лобарев Андрей Александрович 5302
 // Модуль N-13 (gcf_nn_n) Нахождение НОД натуральных чисел
 
-function gcf_nn_n(a ,b) {
+MathLib.gcf_nn_n = function(a ,b) {
 
   var tmp;                      // Переменная для обмена местами a и b в случае, если a < b
 
@@ -10,23 +10,27 @@ function gcf_nn_n(a ,b) {
     case 0: return b;             // 0 - числа равны, возвращаем b
 
     case 1:                       // 1 - a < b, меняем местами a и b
-      tmp = a;
-      a = b;
-      b = tmp;
+            tmp = a;
+            a = b;
+            b = tmp;
 
     case 2:                       // 2 - a > b, переходим к алгоритму Евклида
-      var q;                      // переменная для хранения последнего остатка
+            var q;                      // переменная для хранения последнего остатка
 
-      while (1) {
-        q = mod_nn_n(a, b);       // берем остаток от a / b
+            while (1) {
+              q = mod_nn_n(a, b);       // берем остаток от a / b
 
-        if (nzer_n_b(q)) {        // проверяем остаток на равенство нулю
-          a = b;                  // если не равен, переходим к следующему
-          b = q;                  // шагу алгоритма Евклида
-        }
-        else return b;            // если остаток равен нулю, возвращаем b
-      }
+              if (nzer_n_b(q)) {        // проверяем остаток на равенство нулю
+                a = b;                  // если не равен, переходим к следующему
+                b = q;                  // шагу алгоритма Евклида
+              }
+              else return b;            // если остаток равен нулю, возвращаем b
+            }
 
-    default: console.log("in function gcf_nn_n: function com_nn_d has returned an incorrect value"); // если com_nn_d возвращает неверное значение,
-  }                                                                                                  // выводим сообщение
+    default: 
+            console.log("in function gcf_nn_n: function com_nn_d \
+                has returned an incorrect value"); 
+              // если com_nn_d возвращает неверное значение,
+              // выводим сообщение
+  }        
 }
