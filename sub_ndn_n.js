e0bbf306b2357b20a3764eaa93ed1206889c4519
paulbilkis <jew.bilkis@gmail.com>
2016-04-21 21:01:47 +0300

    fix a bug in sub_ndn_n.js, integrated sub_ndn_n and mul_nn_n

--- sub_ndn_n.js
+++ sub_ndn_n.js
@@ -1,7 +1,10 @@
 /* Вычитание из натурального другого натурального,
 умноженного на цифру для случая с неотрицательным результатом */
 // Исмаилов Т.Э. 5301
-MathLib.sub_ndn_n = function(n1, d, n2) {
+MathLib.sub_ndn_n = function(num1, d, num2) {
+	
+	n1 = MathLib.cloneNumber (num1);
+	n2 = MathLib.cloneNumber (num2);
 
   if( MathLib.com_nn_d(n1, n2) != 1 ) { // n1 >= n2
     n2 = MathLib.mul_nd_n(n2, d); // умножаем второе длинное число на цифру f
