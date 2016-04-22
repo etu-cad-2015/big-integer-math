f4e7e49a31eeb7c3b79d74b1e002a6dffaffabb9
paulbilkis <jew.bilkis@gmail.com>
2016-04-23 01:47:42 +0300

    fixed a bug in add_zz_z

--- add_zz_z.js
+++ add_zz_z.js
@@ -10,16 +10,16 @@ MathLib.add_zz_z = function (n1, n2){
     if (a!=1 && b!=1){
 	return MathLib.add_nn_n(n1, n2);
     }else if (a!=1){
-	if (MathLib.com_nn_d (n1, n2)==2){
+	if (MathLib.com_nn_d (n1, n2)!=1){
 	    return MathLib.sub_nn_n(n1, MathLib.abs_z_n(n2));
 	}else {
-	    return MathLib.mul_zm_z(MathLib.sub_nn_n(n1, MathLib.abs_z_n(n2)));
+	    return MathLib.mul_zm_z(MathLib.sub_nn_n(MathLib.abs_z_n(n2), n1));
 	}
     }else if (b!=1){
-	if (MathLib.com_nn_d (n1, n2)==2){
-	    return MathLib.mul_zm_z(MathLib.sub_nn_n(n2, MathLib.abs_z_n(n1)));
+	if (MathLib.com_nn_d (n1, n2)!=1){
+	    return MathLib.mul_zm_z(MathLib.sub_nn_n(MathLib.abs_z_n(n1), n2));
 	}else {
-	    return MathLib.sub_nn_n(n2, Mathlib.abs_z_n(n1));
+	    return MathLib.sub_nn_n(n2, MathLib.abs_z_n(n1));
 	}
 	
     }else{
