e0751738d3b0ab320a3748fe1ab6c8b863638c52
paulbilkis <jew.bilkis@gmail.com>
2016-04-24 16:40:43 +0300

    re-write mul_pp_p. god i hate u guys integrated mul_pp_p

--- mul_pp_p.js
+++ mul_pp_p.js
@@ -1,45 +1,20 @@
-function MUL_PP_P(firstPol, secondPol)
+MathLib.mul_pp_p = function (firstPol, secondPol)
 {
-	var i, len, result;
-	len = firstPol.length+secondPol.length; //Ограничение цикла обнуления (узнаём макс. степень)
-	for (i=0; i<len; i++)					//Обнуляем все коэффициенты
-	{
-		result[i].[0].d=0;					//Числитель=0
-		result[i].[1].d=1;					//Знаменатель=1
-		result[i].[0].s=1;					//Знак числителя: +
+	var i, len, result = [];
+    len = MathLib.deg_p_n(firstPol) + MathLib.deg_p_n(secondPol);
+    result = [];
+    for (i=0; i<=len; i++)					//Обнуляем все коэффициенты
+    {
+	 result[i] = MathLib.cloneFraction (MathLib.ZERO_FRACTION);
+    }
+
+    for (var i=0; i<=len; i++){
+	for (var j=0; j<=i; j++){
+	    b = i-j;
+	    result[i] = MathLib.add_qq_q(MathLib.getPolynomCoefficient(result, i), MathLib.mul_qq_q(MathLib.getPolynomCoefficient(firstPol, j), MathLib.getPolynomCoefficient(secondPol, b)));
 	}
-	//Начинаем вычислять результат
-	if (firstPol.length>=secondPol.length)	//Если степень первого больше или равна
-		for (i=0; i<secondPol.lenght; i++)
-		{
-			result=ADD_PP_P( MUL_P_Q(MUL_Pxk_P(firstPol,i),secondPol[i]));
-		}
-	else									//Если степень второго больше
-		for (i=0; i<firstPol.length; i++)	
-		{
-			result=ADD_PP_P( MUL_P_Q(MUL_Pxk_P(secondPol,i),firstPol[i]));
-		}
-	return result;
-}
-//Вычисление:
-//ADD_PP_P - добавляем к существующим коэффициентом коэффициенты нового многочлена
-//MUL_P_Q - умножаем все коэффициенты  первого многочлена на коэффициент второго
-//стоящего при x^i
-//MUL_Pxk_P предварительно домножаем на x^i
-//
-//На примере (x^2-1)*(x+1)
-//Степень первого больше.
-//firstPol= -1*x^0 + 0*x^1 + 1*x^2
-//secondPol= 1*x^0 + 1*x^1
-//result= 0*x^0 + 0*x^1 + 0*x^2 + 0*x^3
-//Цикл пойдёт два раза
-//1 ПРОХОД------------------------------------------------------------------------------
-//MUL_Pxk_P(firstPol,0) = -1*x^0 + 0*x^1 + 1*x^2
-//MUL_P_Q(MUL_Pxk_P(firstPol,0),secondPol[0]) = -1*x^0 + 0*x^1 + 1*x^2
-//ADD_PP_P( MUL_P_Q(MUL_Pxk_P(secondPol,0),firstPol[0])) = -1*x^0 + 0*x^1 + 1*x^2
-//2 ПРОХОД------------------------------------------------------------------------------
-//MUL_Pxk_P(firstPol,1) = -1*x^1 + 0*x^2 + 1*x^3
-//MUL_P_Q(MUL_Pxk_P(firstPol,1),secondPol[1]) = -1*x^1 + 0*x^2 + 1*x^3
-//ADD_PP_P( MUL_P_Q(MUL_Pxk_P(secondPol,1),firstPol[1])) = -1*x^0 + -1*x^1 + 1*x^2 + 1*x^3
-//
-//Возвращаем результат
+    }
+
+    return result;
+};
+    
