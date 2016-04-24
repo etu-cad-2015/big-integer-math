e027da4ddc7b29a3c516da233a7a4d442975c056
3angel <hwguy.siplus@gmail.com>
2016-04-24 22:27:39 +0300

    Rewrite der_p_p

--- der_p_p.js
+++ der_p_p.js
@@ -1,19 +1,18 @@
-﻿//Кольцов С.О. 5301
- //Производная многочлена
- 
- function DER_P_P(polynom)
- {
- 	var carry = 0;//перенос разряда
- 	//последовательно проходим по каждому числителю многочлена
- 	for(i=0 ; i<polynom.length ; i++)
+//Кольцов С.О. 5301
+//Производная многочлена
+MathLib.der_p_p = function(polynom)
+{
+	polynom.shift(); //удаляем первый элемент, ибо его степень 0
+
+	// последовательно проходим по каждому члену многочлена
+	for (var i = 0; i < polynom.length; i++)
 	{
- 		//умножаем числитель каждого многочлена на его степень i
- 		for(j = 0 ; j < polynom.d.length ; j++)
- 		{
- 			polynom[i][0].d[j] = (polynom[i][0].d[j] * i + carry) % p;
- 			carry = (polynom[i][0].d[j] * i + carry) % p;//считаем перенос
- 		}
- 	}
- 	polynom.shift();//удаляем первый элемент, ибо он 0
- 	return(polynom);
- }
+		// умножаем числитель каждого многочлена на его степень i
+		var coeff = polynom[i];
+		if (coeff != null) {
+			polynom[i] = MathLib.mul_qq_q(coeff, MathLib.trans_z_q(MathLib.jsNumberToNumber(i + 1)));
+		}
+	}
+
+	return polynom;
+}
