9bf0db91b83e997feb77861fc4dfe8026e617fc4
paulbilkis <jew.bilkis@gmail.com>
2016-04-25 01:35:01 +0300

    integrated der_p_p. edited mod, nmw and gcf, don't know if they working or not, cause div_pp_p is still looping the hell am i the only one here who's doing something?

--- gcf_pp_p.js
+++ gcf_pp_p.js
@@ -1,21 +1,19 @@
-﻿function gcf_pp_p
-{
+﻿MathLib.gcf_pp_p = function (a, b){
   // нод многочленов
   //Родионова К. 5301
 
 
 //цикл работает когда степень a>=b
-if (deg_p_n (a)>= deg_p_n (b)
+    if (MathLib.deg_p_n (a)>= MathLib.deg_p_n (b))
 {
 
-   while (mod_pp_p (a, b) != 0) //работает пока остаток от деления не равна нулю
-    {int tmp = mod_pp_p (a, b); //записываем остаток от деления a%b
+   while (MathLib.mod_pp_p (a, b) != 0) //работает пока остаток от деления не равна нулю
+    {
+	var tmp = MathLib.mod_pp_p (a, b); //записываем остаток от деления a%b
          a=b;
          b=tmp;
     }
    return b;
   
-else
-    
-     return 0;      
-   
+}else return 0;      
+};
