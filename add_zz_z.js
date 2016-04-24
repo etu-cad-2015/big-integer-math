9e85b9cdef193136827f0a9f9399e7f92930f54f
3angel <hwguy.siplus@gmail.com>
2016-04-24 22:07:37 +0300

    Anton Mazur: Update add_zz_z

--- add_zz_z.js
+++ add_zz_z.js
@@ -1,31 +1,33 @@
-MathLib.abs_z_n = function (n){
-    n1 = MathLib.cloneNumber(n);
-    n1.s = 1;
-    return n1;
-};
-  
-MathLib.add_zz_z = function (n1, n2){
-    var a = MathLib.poz_z_d (n1);
-    var b = MathLib.poz_z_d (n2);
-    if (a!=1 && b!=1){
-	return MathLib.add_nn_n(n1, n2);
-    }else if (a!=1){
-	if (MathLib.com_nn_d (n1, n2)!=1){
-	    return MathLib.sub_nn_n(n1, MathLib.abs_z_n(n2));
-	}else {
-	    return MathLib.mul_zm_z(MathLib.sub_nn_n(MathLib.abs_z_n(n2), n1));
+// Сложение целых чисел.
+// -- Мазур Антон, 5302.
+MathLib.add_zz_z = function (n1, n2) {
+	// signs of each term
+	var a = MathLib.poz_z_d(n1);
+	var b = MathLib.poz_z_d(n2);
+	if (a != 1 && b != 1) {
+		// both numbers are not negative
+		return MathLib.add_nn_n(n1, n2);
 	}
-    }else if (b!=1){
-	if (MathLib.com_nn_d (n1, n2)!=1){
-	    return MathLib.mul_zm_z(MathLib.sub_nn_n(MathLib.abs_z_n(n1), n2));
-	}else {
-	    return MathLib.sub_nn_n(n2, MathLib.abs_z_n(n1));
+	else {
+		// the first number is positive or zero, the second is negative
+		if (a != 1) {
+			if (MathLib.com_nn_d(n1, n2) != 1) {
+				return MathLib.sub_nn_n(n1, n2);
+			} else {
+				return MathLib.mul_zm_z(MathLib.sub_nn_n(n2, n1));
+			}
+		} else {
+			// the second number is positive or zero, the first is negative
+			if (b!=1) {
+				if (MathLib.com_nn_d(n1, n2) != 1) {
+					return MathLib.mul_zm_z(MathLib.sub_nn_n(n1, n2));
+				} else {
+					return MathLib.sub_nn_n(n2, n1);
+				}
+			} else {
+				// two numbers are negative
+				return MathLib.mul_zm_z(MathLib.add_nn_n(n1, n2));
+			}
+		}
 	}
-	
-    }else{
-	return MathLib.mul_zm_z(MathLib.add_nn_n(MathLib.abs_z_n(n1), MathLib.abs_z_n(n2)));
-    }
 };
-        
-
-  	
