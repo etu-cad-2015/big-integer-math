264ac0dcf9a135d1850ebaaf62a5364e8d7dcc47
paulbilkis <jew.bilkis@gmail.com>
2016-04-24 03:18:29 +0300

    fixed and integrated add_pp_p

--- add_pp_p.js
+++ add_pp_p.js
@@ -3,18 +3,13 @@
 //Кашинцев А.А. , 5301
 MathLib.add_pp_p = function (polinom1, polinom2) {
 
-      //суммарный многочлен
-      var polinomsum = [];
 	  
       //Нахождение длины наибольшего многочлена (кол-во итераций будущего цикла)
       var maxlength = Math.max(polinom1.length, polinom2.length);
 	  
 	  for (var i = 0; i < maxlength; ++i) {
-	        //Добавление нового элемента в массив коэффициентов полинома polinomsum 
-	        polinomsum.push ([ { d: [], s: 1 }, { d: [], s: 1 ]); 
-		//Вычисление i-ого коэффициента суммарного многочлена
-		polinomsum[i] = MathLib.add_qq_q (polinom1[i], polinom2[i]);
-	        }
-			
-      return polinomsum;	
-}
+
+	      polinom1[i] = MathLib.add_qq_q (MathLib.getPolynomCoefficient(polinom1, i), MathLib.getPolynomCoefficient(polinom2, i));
+	        }		
+      return polinom1;	
+	  };
