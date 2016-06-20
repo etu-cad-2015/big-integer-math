67e8c788f9963c869e41f146e8c73f60bcda7da9
unknowdigi <stewedfruit13y@gmail.com>
2016-06-20 16:56:33 +0300

    Update nmw_p_p.js

--- nmw_p_p.js
+++ nmw_p_p.js
@@ -2,25 +2,11 @@
 //Преобразование многочлена - кратные корни в простые
 MathLib.nmw_p_p = function(polinom)
 {
-	//просто вспомогательные переменные
-	var z, m, r;
-	//результатирующий полином
-	var p = [ MathLib.cloneFraction(MathLib.ONE_FRACTION) ];
+	var z, r;
 	
-	//пока степень исходно полинома не равна 0
-	while(MathLib.deg_p_n(polinom) != 0)
-	{
-		//r - НОД исходного полинома и его производной
-		r = MathLib.gcf_pp_p(polinom, MathLib.der_p_p(polinom));
-		//z - частное исходного полинома и r
-		z = MathLib.div_pp_p(polinom, r);
-		//m - НОД z и производной
-		m = MathLib.gcf_pp_p(z, MathLib.der_p_p(polinom));
-		//при делении z на m получаем линейный множитель в 1ой степени
-		//произведение таких множителей и есть исходный полином
-		p = MathLib.mul_pp_p(p, MathLib.div_pp_p(z, m));
-		polinom = r;
-	}
+	r = MathLib.gcf_pp_p(Object.assign([], polinom), MathLib.der_p_p(Object.assign([], polinom)));
+	z = MathLib.div_pp_p(Object.assign([], polinom), Object.assign([], r));
+	z = MathLib.mul_pq_p(Object.assign([], z), MathLib.led_p_q(Object.assign([], r)));
 	
-	return MathLib.finalizePolynom(p);
+	return MathLib.finalizePolynom(z);
 };
